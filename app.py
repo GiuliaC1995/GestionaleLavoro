457995545028a57b6cee98a77c0e5aaaa6e85f41
Giulia Calvino <163443796+GiuliaC1995@users.noreply.github.com>
2025-10-07 18:07:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -289,7 +289,7 @@ macro_tipologia_attivita = {
     "RICERCA": {
         "Articolo scientifico": ["Scrittura", "Revisione", "Sottomissione"],
         "Riunioni e attività amministrative": ["Riunioni e attività amministrative"],
-        "Studio e analisi"
+        "Studio e analisi": ["Studio articoli","Analisi dei dati"]
     }
 }
 
@@ -1306,6 +1306,7 @@ if st.sidebar.button("🚪 Logout", key="logout_common"):
     st.session_state.username = ""
     st.session_state.ruolo = ""
     st.rerun()
+
 
 
 
